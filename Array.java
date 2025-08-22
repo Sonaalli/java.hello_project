eefc401fe2c09cc33f968c2576d1fc358977476f
Sonaalli <121749037+Sonaalli@users.noreply.github.com>
2025-08-22 16:28:33 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,203 @@
+//import java.util.*;
+//public class Array{
+  //  public static void main(String args[]){
+        
+//Scanner sc= new Scanner(System.in);
+//int n = sc.nextInt();
+//int arr[]=new int[n];
+//System.out.println("enter the elements of the array:");
+
+//for(int i=0; i<n; i++){
+
+  //   arr[i]=sc.nextInt();
+//}
+//System.out.println("enter the target value:");
+//int target=sc.nextInt();
+
+//int result[]=findTwoSum(arr,target);
+
+//if(result[0]!=-1){
+   // System.out.println("Indices of the two numbers:"+result[0]+"and"+result[1]);
+//}
+//else{
+  //  System.out.println("no two numbers add up to the target:");
+//}
+
+//}
+
+//public static int[] findTwoSum(int arr[],int target){
+    //HashMap<Integer, Integer>map=new HashMap<>();
+
+    //for(int i=0; i<arr.length; i++){
+        //int complement=target-arr[i];
+
+        //if(map.containsKey(complement)){
+          //  return new int[]{map.get(complement),i};
+        //}
+
+      //  map.put(arr[i],i);
+    //}
+  //  return new int[]{-1,-1};
+//}
+        
+    //}
+
+//import java. util.*;
+//public class Array{
+    //public static int[] findTwoSum(int arr[],int target){
+        //for(int i=0; i<arr.length; i++){
+            //for(int j=i; j<arr.length; j++){
+
+            //    if(arr[i]+arr[j]==target){
+          //          return new int[] {i,j};
+        //        }
+      //      }
+    //    }
+  //      return new int[]{-1,-1};
+
+
+//}
+        
+    
+
+    //public static void main(String args[]){
+        //Scanner sc=new Scanner(System.in);
+        //int n=sc.nextInt();
+        //int arr[]=new int[n];
+
+        //for(int i=0;i<n;i++){
+          //  arr[i]=sc.nextInt();
+        //}
+        //int target=sc.nextInt();
+        //int result[]=findTwoSum(arr, target);
+
+        //if(result[0]!=-1){
+          //  System.out.println("Indices of the two numbers:"+result[0]+"and"+result[1]);
+
+        //}
+        //else{
+      //      System.out.println(" no two numbers add up to the target");
+    //    }
+  //  }
+//}
+
+
+
+//import java.util.*;
+//public class Array{
+    //public static void main(String args[]){
+        //Scanner sc= new Scanner(System.in);
+        //int size=sc.nextInt();
+        //String[] arr=new String[size];
+
+        //for(int i=0; i<size; i++){
+          //  arr[i]=sc.next().CharAt(0);
+        //}
+        //int vowels=0, consonants=0;
+
+        //for(char ch:arr){
+            //ch=Character.toLowerCase(ch);
+            //if(ch>='a'&&ch>='z'){
+                //if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                  //  vowels++;
+                //}
+                //else{
+              //      consonants++;
+            //    }
+          //  }
+        //}
+      //  System.out.println(" vowels number"+vowels);
+    //    System.out.println(" consonants number"+consonants);
+  //  }
+//}
+
+    
+//import java.util.*;
+
+//public class Array{
+  //public static void main(String args[]){
+   /// String str="Automation";
+
+   // int vowels=0;
+    //int consonants=0;
+
+    //for(int i=0; i<str.length(); i++){
+      //char c=str.charAt(i);
+
+      //if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+         //c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+          //vowels++;
+         //}
+
+         //else if(Character.isLetter(c)){
+        //  consonants++;
+
+      //   }
+
+         
+
+    //}
+    //System.out.println("Vowels"+ vowels);
+    //System.out.println("Consonants"+ consonants);
+  //}
+  
+//}
+
+//import java.util.*;
+//public class Array{
+  //public static void main(String args[]){
+    
+    
+  //}
+//}
+
+/*import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int size= sc.nextInt();
+    sc.nextline();
+
+    String arr[] =new String[size];
+
+    for(int i=0; i<size; i++){
+      arr[i]=sc.nextInt();
+    }
+
+    for(String str:arr){
+      int vowels=0, consonants=0;
+      str=str.toLowerCase();
+
+
+      for(int i=0; i<str.length(); i++){
+        char ch=str.charAt(i);
+
+        
+
+        
+
+        
+      }
+    }
+  }
+}*/
+
+
+
+/*public class Array{
+  public static void main(String args[]){
+    
+    int arr[]={23,4,5,7,8};
+    int largest=arr[0];
+    for(int i=0; i<arr.length; i++){
+      if(arr[i]>largest){
+        largest=arr[i];
+      }
+    }
+      System.out.println("largest element is"+largest);
+
+    
+  }
+
+}*/
+
